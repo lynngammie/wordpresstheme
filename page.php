43e2c8f0b02f48bdd35bcb022b4c2ef193804719
Lynn Gammie <gammie.lynn@gmail.com>
2016-03-05 18:06:13 -0500

    books page preliminary styling

--- page.php
+++ page.php
@@ -1,21 +1,34 @@
 <?php get_header();  ?>
 
 <div class="main">
-  <div class="container">
-
-    <div class="content">
+  <?php if (has_post_thumbnail( $post->ID ) ): ?>
+  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+  <?php endif; ?>
+    <div style="background-image:url(<?php echo $image[0]; ?>)" class="page-content">
+    </div>
+      <div class="page-container">
       <?php // Start the loop ?>
-      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+        <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
+          <!-- <h2><?php  
+          $title = get_the_title();
+          echo $title;?></h2> -->
 
-      <?php endwhile; // end the loop?>
-    </div> <!-- /,content -->
+          <p><?php the_content(); ?></p>
 
-    <?php get_sidebar(); ?>
+        <?php endwhile; // end the loop?>
+        <div class="books-container">
+          <img src="<?php the_field('book_cover'); ?>" alt="">
+          <img src="<?php the_field('book_cover_2'); ?>" alt="">
+          <img src="<?php the_field('book_cover_3'); ?>" alt="">
+          <img src="<?php the_field('book_cover_4'); ?>" alt="">
+          <img src="<?php the_field('book_cover_5'); ?>" alt="">
+          <img src="<?php the_field('book_cover_6'); ?>" alt="">
+        </div>
 
-  </div> <!-- /.container -->
+    </div> <!-- /.container -->
+
+
 </div> <!-- /.main -->
 
 <?php get_footer(); ?>
